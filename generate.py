7cb7c8f686e9463fa45e5ac9de618271f5cf9d0c
Toph Allen <toph.allen@gmail.com>
2020-01-30 15:15:02 -0500

    Rewrote cleaning function

--- generate.py
+++ generate.py
@@ -1,42 +1,20 @@
 import pandas as pd
 import numpy as np
-import geopandas as gpd
-from scipy.spatial import Voronoi, voronoi_plot_2d
-import matplotlib.pyplot as plt
-import shapely
+from scipy.spatial import Voronoi
+from shapely.geometry import Polygon, MultiPolygon
+from shapely.ops import unary_union
+from tqdm.notebook import tqdm #TODO: make this conditional on verbose option
 
-# Get the polygons for a country
-def generate_voronoms(country, admin_level, geonames, shapes):
-    # Get the polygons for a country
-    admin_cols = ["admin{}_code".format(i) for i in range(1, admin_level + 1)]
 
-    # Get the admin geonames we'll be finding polygons for.
-    admin_predicate = "country_code == '{}' & feature_class == 'A' & feature_code == 'ADM{}'".format(country, admin_level)
-
-    admin_geonameids = (
-        geonames.query(admin_predicate).loc[:, admin_cols].
-        drop_duplicates().dropna()
-    ).index
-    admin_geonames = geonames.loc[admin_geonameids]
-
-    # Get the geonames we'll use for the Voronoi diagram.
-    # We only draw the Voronoi diagram with GeoNames which directly
-    # have our admin level of interest listed in their admin2_code column.
-    # This is because:
-    # 1. GeoNames have multiple levels of hierarchy assigned, but we can't be sure that they are coherent between levels.
-    # 2. So we have to draw the Voronoi diagram at one level of hierarchy.
-    # 3. Drawing the Voronoi diagram is fairly costly, so we want to do it once, so;
-    # 4. We want to get all geonames with *this* level of admin hierarchy in *this* country to draw our voronoi diagram.
-    voronoi_predicate_parts = ["country_code == '{}'".format(country)]
-    for col in admin_cols:
-        voronoi_predicate_parts.append("{} in @admin_geonames.{}".format(col, col))
-    voronoi_predicate = " & ".join(voronoi_predicate_parts)
-
-    voronoi_geonames = geonames.query(voronoi_predicate)
+def admin_polygons(country, admin_level, geonames, shapes, clean=None):
+    admin_geonames = get_admin_geonames(country, admin_level, geonames)
+    voronoi_geonames = get_voronoi_geonames(country, admin_level, geonames)
 
     # Get the outline for the country we're handling
     shapes["country_code"] = geonames.loc[shapes.index, "country_code"]
-    country_outline = shapes.query("country_code == '{}'".format(country)).iloc[0]["geometry"]
+    country_outline = shapes.query("country_code == '{}'".format(country)).iloc[0][
+        "geometry"
+    ]
 
     # Draw the Voronoi diagram
     print("Creating Voronoi diagram...")
@@ -45,67 +23,224 @@ def generate_voronoms(country, admin_level, geonames, shapes):
 
     # Polygon-generating loop
     admin_polygons = []
-    for geonameid, admin_geoname in tqdm(admin_geonames.iterrows(), total = len(admin_geonames)):
-        print("Working on {}...".format(admin_geoname.name))
-        # Get the indices of the voronoi regions in this admin area
-        polygon_predicate_parts = ["country_code == '{}'".format(country)]
-        for col in admin_cols:
-            polygon_predicate_parts.append("{} == @admin_geoname.{}".format(col, col))
-        polygon_predicate = " & ".join(polygon_predicate_parts)
-
-        polygon_geonames = geonames.query(polygon_predicate)
-        polygon_geonameids = polygon_geonames.index
-
-        # Figure out which Voronoi regions we need.
-        # The Voronoi module calls them "regions" but I'm calling them
-        # "cells" for clarity's sake.
-        point_indices = np.where(voronoi_geonames.index.isin(polygon_geonameids))[0]
-        cell_indices = country_voronoi.point_region[point_indices]
-
-        # Get the points for the regions and make them into Polygons
-        admin_voronoi_cells = []
-        for cell_idx in cell_indices:
-            vert_indices = country_voronoi.regions[cell_idx]
-            if -1 in vert_indices:
-                continue
-            cell = shapely.geometry.Polygon(
-                [country_voronoi.vertices[vert_idx] for vert_idx in vert_indices]
+    print("Generating polygons...")
+    for geonameid, admin_geoname in tqdm(admin_geonames.iterrows(), total=len(admin_geonames)):
+        admin_polygons.append(
+            extract_polygon_from_voronoi(
+                admin_geoname, geonames, voronoi_geonames, country_voronoi, country_outline
             )
-            admin_voronoi_cells.append(cell)
-        """
-        We take the MultiPolygon made from the Voronoi cells in an admin region
-        and run the following operations:
-            1. Removes any Voronoi cells with no neighbors. This should be improved.
-            2. Uses Shapely's unary union function to join all the polygons.
-            3. Intersects the resulting shape with the country's outlines.
-        """
-        # TODO: COMMENT OUT THIS BLOCK AND TRY RUNNING FOR US STATES
-#         non_singleton_cells = []
-#         for cell in admin_voronoi_cells:
-#                 # Because we don't need to _count_ the number of adjacent regions,
-#                 # we iterate through until we find an adjacent region, and as soon as we
-#                 # find a neighbor we add it to the list of regions to include.
-#             any_neighbors = False
-#             for other_cell in admin_voronoi_cells:
-#                 if cell.touches(other_cell):
-#                     any_neighbors = True
-#                     break
-#             if any_neighbors == True:
-#                 non_singleton_cells.append(cell)
-#         admin_voronoi_cells = non_singleton_cells 
+        )
+    print("Done.")
 
-        if len(admin_voronoi_cells) != 0:
-            admin_multipolygon = shapely.geometry.MultiPolygon(admin_voronoi_cells).buffer(0)
-            admin_polygon = shapely.ops.unary_union(admin_multipolygon).intersection(country_outline)
-            # The following line should actually come after the unary union but before the intersection.
-            # polygon = polygon.simplify(tolerance = 0.02).buffer(0)
-            admin_polygons.append(admin_polygon)
-        else:
-            print("Error with {}.".format(admin_geoname.name))
+    if clean is None:
+        pass
+    elif clean == "cutoff":
+        print("Cleaning polygons...")
+        admin_polygons = clean_polygons_max_diff_cutoff(admin_polygons)
+        print("Done")
+    elif clean == "simple":
+        print("Cleaning polygons...")
+        admin_polygons = clean_polygons_simple(admin_polygons)
+        print("Done.")
+
+    return admin_polygons
+
+
+def admin_cols(admin_level=None):
+    return ["admin{}_code".format(i) for i in range(1, admin_level + 1)]
+
+
+def get_admin_geonames(country, admin_level, geonames):
+    """
+    This function returns the GeoNames from a specified admin level in a country.
+    """
+
+    # Get the admin geonames we'll be finding polygons for.
+    admin_predicate = "country_code == '{}' & feature_class == 'A' & feature_code == 'ADM{}'".format(
+        country, admin_level
+    )
+
+    admin_geonameids = (
+        geonames.query(admin_predicate).loc[:, admin_cols(admin_level)].drop_duplicates().dropna()
+    ).index
+    admin_geonames = geonames.loc[admin_geonameids]
+    return admin_geonames
+
+
+def get_voronoi_geonames(country, admin_level, geonames):
+    """
+    This function returns all GeoNames which have an admin code at the specified level, and
+    thus can be used to make inferences about the admin area shapes.
+    """
+    # TODO: This is super inefficient.
+    admin_geonames = get_admin_geonames(country, admin_level, geonames)
+    voronoi_predicate_parts = ["country_code == '{}'".format(country)]
+    for col in admin_cols(admin_level):
+        voronoi_predicate_parts.append("{} in @admin_geonames.{}".format(col, col))
+    voronoi_predicate = " & ".join(voronoi_predicate_parts)
+
+    voronoi_geonames = geonames.query(voronoi_predicate)
+    return voronoi_geonames
+
+
+def extract_polygon_from_voronoi(admin_geoname, geonames, voronoi_geonames, country_voronoi, country_outline):
+    # Get the indices of the voronoi regions in this admin area
+    admin_col_names = [col for col in admin_geoname.index if "admin" in col]
+    polygon_predicate_parts = ["country_code == '{}'".format(admin_geoname.country_code)]
+    for col in admin_col_names:
+        if not pd.isnull(admin_geoname[col]):
+            polygon_predicate_parts.append("{} == '{}'".format(col, admin_geoname[col]))
+    polygon_predicate = " & ".join(polygon_predicate_parts)
+    polygon_geonames = geonames.query(polygon_predicate)
+    polygon_geonameids = polygon_geonames.index
+
+    # Figure out which Voronoi regions we need.
+    # The Voronoi module calls them "regions" but I'm calling them
+    # "cells" for clarity's sake.
+    point_indices = np.where(voronoi_geonames.index.isin(polygon_geonameids))[0]
+    cell_indices = country_voronoi.point_region[point_indices]
+
+    # Get the points for the regions and make them into Polygons
+    admin_cells = []
+    for cell_idx in cell_indices:
+        vert_indices = country_voronoi.regions[cell_idx]
+        if -1 in vert_indices:
             continue
-    try:
-        admin_geonames["geometry"] = admin_polygons
-        return admin_geonames
-    except error:
-        print("Could not return geonames DataFrame; returning list instead.")
-        return admin_polygons
+        cell = Polygon(
+            [country_voronoi.vertices[vert_idx] for vert_idx in vert_indices]
+        )
+        admin_cells.append(cell)
+    # The unary union is the slow part.
+    unified_cells = unary_union(MultiPolygon(admin_cells)).intersection(country_outline)
+    if type(unified_cells) is Polygon:
+        unified_cells = MultiPolygon([unified_cells])
+    return unified_cells
+
+
+# Cleaning functions
+
+def clean_polygons_simple(admin_polygons):
+    """
+    Deletes any part of an admin polygon that is entirely
+    contained within another admin area in the country.
+    """
+    # Fill in holes in the polygons
+    filled_admin_polygons = []
+    for admin_polygon in admin_polygons:
+        filled_poly = [
+            Polygon(poly.exterior) for poly in admin_polygon
+        ]
+        filled_admin_polygons.append(MultiPolygon(filled_poly))
+
+    # Remove any polygons that are contained inside the now-filled polygons
+    cleaned_admin_polygons = []
+    for this_area in tqdm(filled_admin_polygons):
+        # Make a flat list of subpolygons of other admin regions
+        other_polys = []
+        for other_area in (filled_admin_polygons):
+            if other_area is not this_area:
+                for poly in other_area:
+                    other_polys.append(poly)
+
+        not_within_others = []
+        for poly in this_area:
+            if not any([poly.within(other_poly) for other_poly in other_polys]):
+                not_within_others.append(poly)
+        new_area = MultiPolygon(not_within_others)
+        cleaned_admin_polygons.append(new_area)
+    return cleaned_admin_polygons
+
+
+def clean_polygons_max_diff_cutoff(admin_polygons):
+    """
+    Separates polygons in each admin area into "must keep" and "can delete" bins based on the largest cutoff of differences between sizes. "Can delete" polygons will be deleted if they are contained by another admin area. "Must keep" polygons will be kept, and will cut out holes in admin areas that contain them.
+
+    This should work better for single-area admin areas which are entirely contained in other regions.
+
+    It will likely fail for admin areas with no clear cutoff, or with multiple groups of sizes. More complex heuristics, such as k-means clustering, or an analysis of the entire group of admin areas to determine the best route, are probably a good aim for the next version.
+    """
+    # Fill in holes in the polygons
+    filled_polygons = []
+    for admin_polygon in admin_polygons:
+        filled_poly = [
+            Polygon(poly.exterior) for poly in admin_polygon
+        ]
+        filled_polygons.append(MultiPolygon(filled_poly))
+
+    # Separate polygons per region into "must keep" and "can delete". 
+    separated_polygons = [separate_deletion_candidates(p) for p in filled_polygons]
+    must_keep = [x[0] for x in separated_polygons]
+    can_delete = [x[1] for x in separated_polygons]
+
+    # First we'll iterate through the polygons we HAVE to keep. We'll compare
+    # each one to each other polygon we have to keep. If it contains another
+    # must-keep polygon, we'll cut that out from it. Then we'll add the
+    # resulting shape to our new list.
+    cleaned_polygons = []
+    for this_area in tqdm(must_keep):
+        other_polys = []
+        for other_area in must_keep:
+            if other_area is not this_area:
+                for poly in other_area:
+                    other_polys.append(poly)
+        new_area = []
+        for a in this_area:
+            for b in other_polys:
+                if a.contains(b):
+                    a = a.difference(b)
+            new_area.append(a)
+        cleaned_polygons.append(new_area)
+
+    # Next we'll operate on the polys which we can delete if they're contained.
+    # This will operate in two stages.
+
+    # first, deleting any which are contained in a must-keep area.
+    first_pass_kept = []
+    for this_area in tqdm(can_delete):
+        # We'll only draw polygons from the must_keep areas at first.
+        keep_polys = []
+        for other_area in cleaned_polygons:
+            for poly in other_area:
+                keep_polys.append(poly)
+        not_contained = []
+        for poly in this_area:
+            if not any([poly.within(other_poly) for other_poly in other_polys]):
+                not_contained.append(poly)
+        first_pass_kept.append(not_contained)
+
+    # Second, deleting any which are contained in another candidate area.
+    for i, this_area in tqdm(enumerate(first_pass_kept), total = len(first_pass_kept)):
+        other_polys = [] # We'll only draw polygons from the must_keep areas at first.
+        for other_area in first_pass_kept:
+            if other_area is not this_area:
+                for poly in other_area:
+                    other_polys.append(poly)
+        not_contained = []
+        for poly in this_area:
+            if not any([poly.within(other_poly) for other_poly in other_polys]):
+                not_contained.append(poly)
+        cleaned_polygons[i].extend(not_contained)
+
+    new_admin_areas = [MultiPolygon(area) for area in cleaned_polygons] 
+    return new_admin_areas
+
+
+def create_delete_eligibility_mask(polys):
+    if len(polys) == 1:
+        return [False]
+    areas = [poly.area for poly in polys]
+    sorted_areas = np.sort(areas)
+    area_diffs = np.diff(sorted_areas)
+    max_diff = np.argmax(area_diffs)
+    max_delete = sorted_areas[max_diff]
+    return np.less_equal(areas, max_delete)
+
+
+def separate_deletion_candidates(admin_polygon):
+    if len(admin_polygon) == 0:
+        return ([], [])
+    admin_polygon = np.array(admin_polygon)
+    mask = create_delete_eligibility_mask(admin_polygon)
+    must_keep = admin_polygon[np.invert(mask)]
+    can_delete = admin_polygon[mask]
+    return must_keep, can_delete
--- plot.py
+++ plot.py
@@ -3,7 +3,7 @@ from descartes import PolygonPatch
 from . import colormap
 
 
-def plot_polygon(poly, name=None):
+def polygon(poly, name=None):
     fig = plt.figure(figsize=(6, 6), dpi=300)
     ax = fig.add_subplot(111)
     ax.axis("equal")
@@ -14,15 +14,19 @@ def plot_polygon(poly, name=None):
     plt.show()
 
 
-def plot_polygons(polys, name=None, xlim=None, ylim=None, figsize=(12, 12), dpi=300):
+def polygons(polys, name=None, xlim=None, ylim=None, figsize=(12, 12), dpi=300):
     colors = colormap.rescale_and_interpolate(
         colormap.turbo_colormap_data, range(0, len(polys))
     )
     fig = plt.figure(figsize=figsize, dpi=dpi)
     ax = fig.add_subplot(1, 1, 1)
     ax.axis("equal")
-    for poly, color in zip(polys, colors):
-        ax.add_patch(PolygonPatch(poly, facecolor=color, linewidth=0.1))
+    for i, (poly, color) in enumerate(zip(polys, colors)):
+        try:
+            ax.add_patch(PolygonPatch(poly, facecolor=color, linewidth=0.1))
+        except ValueError as e:
+            print("No value exists for area {}.".format(i))
+            continue
     ax.autoscale()
     ax.set_title(name)
     if xlim is not None:
@@ -32,7 +36,7 @@ def plot_polygons(polys, name=None, xlim=None, ylim=None, figsize=(12, 12), dpi=
     plt.show()
 
 
-def multiplot_polygons(polys, names=None, columns=2, figsize=(12, 24), dpi=300):
+def polygon_subplots(polys, names=None, columns=2, figsize=(12, 24), dpi=300):
     if names is None:
         names = range(len(polys))
     colors = colormap.rescale_and_interpolate(
